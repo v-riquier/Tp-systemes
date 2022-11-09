7ec83ddc3e3399200e8dc531555faede312f6e8a
v-riquier <vincent.riquier80@orange.fr>
2022-11-09 08:41:42 +0100

    TP5 envoi 2

--- Tpcloches/Tpcloches/Tpcloches.cpp
+++ Tpcloches/Tpcloches/Tpcloches.cpp
@@ -4,6 +4,40 @@ Tpcloches::Tpcloches(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+	socket = new QTcpSocket(this);
+	QObject::connect(socket, SIGNAL(connected()), this, SLOT(onSocketConnected()));
+	QObject::connect(socket, SIGNAL(disconnected()), this, SLOT(onSocketDisconnected()));
+}
+
+void Tpcloches::onSocketConnected() {
+	ui.AffConnexion->setText("Connecte");
+}
+
+void Tpcloches::onConnectButtonClicked() {
+	QString ip = ui.IPEdit->text();
+	QString port = ui.PortEdit->text();
+
+	bool ok;
+	int portasint = port.toInt(&ok);
+	if (ok) {
+		socket->connectToHost(ip, portasint);
+	}
+}
+
+void Tpcloches::BtnCloche1Clicked() {
+
+}
+
+void Tpcloches::BtnCloche2Clicked() {
+
+}
+
+void Tpcloches::BtnCloche3Clicked() {
+
+}
+
+void Tpcloches::BtnCloche4Clicked() {
+
 }
 
 Tpcloches::~Tpcloches()
--- Tpcloches/Tpcloches/Tpcloches.h
+++ Tpcloches/Tpcloches/Tpcloches.h
@@ -2,6 +2,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_Tpcloches.h"
+#include <qtcpsocket.h>
 
 class Tpcloches : public QMainWindow
 {
@@ -11,6 +12,15 @@ public:
     Tpcloches(QWidget *parent = nullptr);
     ~Tpcloches();
 
+public slots:
+	void onConnectButtonClicked();
+	void onSocketConnected();
+	void BtnCloche1Clicked();
+	void BtnCloche2Clicked();
+	void BtnCloche3Clicked();
+	void BtnCloche4Clicked();
+
 private:
     Ui::TpclochesClass ui;
+	QTcpSocket * socket;
 };
